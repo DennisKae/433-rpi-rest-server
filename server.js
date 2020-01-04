7e9e7bffa581bd09551744ab7c3e110f098e6e63
DennisKae <DennisKae@users.noreply.github.com>
2020-01-05 00:08:56 +0100

    device_configuration added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Simple Express tutorial: https://www.robinwieruch.de/node-express-server-rest-api
 // Additionally used: nodemon as filechange monitor, swagger-ui-express for the api documentation
 
+'use strict';
 const express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
@@ -10,20 +11,31 @@ const express = require('express'),
     swaggerDocument = require('./swagger.json'),
     swaggerOptions = {
         explorer: false
-    };
+    },
+    fs = require('fs'),
+    deviceConfigurationFile = 'device_configuration.json';
+
+app.listen(port);
+console.log(`API Server started on port ${port}.`);
+console.log('API Docs are located at /api-docs and /swagger');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOptions));
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOptions));
-app.listen(port);
 
-console.log(`API Server started on port ${port}.`);
-console.log('API Docs are at /api-docs and /swagger');
+app.set('x-powered-by', false);
+app.set('json spaces', 2);
 
-app.post('/emit/:value/:length/:delay', (req, res, next) => {
-    const value = req.params.value;
-    const delay = req.params.delay;
-    const length = req.params.length;
-    
+app.use((request, response, next) => {
+    console.log("Requested path: " + request.path);
+    next();
+});
+
+
+app.post('/emit/:value/:length/:delay', (request, response) => {
+    const value = request.params.value;
+    const delay = request.params.delay;
+    const length = request.params.length;
+
     const rfEmitter = rpi433.emitter({
         pin: emitterPin,
         pulseLength: length,
@@ -36,5 +48,12 @@ app.post('/emit/:value/:length/:delay', (req, res, next) => {
     });
     const responseMessage = `Successfully emitted value/length/delay: ${value}/${length}/${delay}`;
     console.log("Response message: " + responseMessage);
-    return res.send(JSON.stringify(responseMessage));
+    return response.send(responseMessage);
+});
+
+app.get('/devices', (request, response, next) => {
+    const rawdata = fs.readFileSync(deviceConfigurationFile);
+    const configuration = JSON.parse(rawdata);
+    response.json(configuration);
+    next();
 });
